a29b72f9edbf3fc4f915ad82b20009a463bda37b
Kyle Tau <kyletau2@gmail.com>
2019-05-26 13:20:40 -0400

    root working

--- api.py
+++ api.py
@@ -9,18 +9,32 @@ elements = [
         "name": "Hydrogen",
         "symbol": "H",
         "atomic number": 1,
-        "atomic mass": 1.00794
+        "atomic mass": 1.00794,
+        "link": "http://www.chemicalelements.com/elements/h.html"
     },
+    {
+        "name": "Helium",
+        "symbol": "He",
+        "atomic number": 2,
+        "atomic mass": 4.002602,
+        "link": "http://www.chemicalelements.com/elements/he.html"
+    }
+
 ]
 
 class Element(Resource):
-    def get(self, name):
+    def get(self, symbol):
         for e in elements:
-            if(name == e["name"]):
+            if(symbol == e["symbol"]):
                 return e, 200
         return "Element not found", 404
 
-api.add_resource(Element, "/elements")
+class All(Resource):
+    def get(self):
+        return elements
+
+api.add_resource(Element, "/elements/<string:symbol>")
+api.add_resource(All, "/")
 
 if __name__ == "__main__":
     app.debug = True
